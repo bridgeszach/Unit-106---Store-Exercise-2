77239e257313de24e7d467ae87a692cc8a5498f5
Zach Bridges <56745560+bridgeszach@users.noreply.github.com>
2020-03-12 11:16:07 -0400

    Add files via upload

---
+++ store/scripts/admin.js
@@ -0,0 +1,26 @@
+var items = [];
+
+function register() {
+    console.log("Current Items" + items.length);
+    var code = $("#code").val();
+    var title = $("#title").val();
+    var price = $('#price').val();
+    var description = $('#description').val();
+    var category = $('#category').val();
+    var image = $('#image').val();
+
+    items.push({
+        code: code,
+        title: title,
+        price: price,
+        description: description,
+        category: category,
+        image: image,
+    });
+
+    console.log("New item:" + items.length);
+}
+
+$("#register-btn").on('click', function () {
+    register();
+});
